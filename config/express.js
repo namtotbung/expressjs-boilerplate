a4588016e4b401a2a38ac4e7afdd8612276e195e
namtotbung <nam.nn265@gmail.com>
2023-09-02 10:59:00 +0700

    Add cors package

--- config/express.js
+++ config/express.js
@@ -1,19 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require("body-parser");
 const routes = require('../routes/indexRoute');
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-  });
 
 app.use('/', routes);
 
